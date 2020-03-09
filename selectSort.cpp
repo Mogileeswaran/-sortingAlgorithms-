eb699dc83b06b51fd8f0dbbae6b6e2f2bb147ef7
Mogileeswaran_M <53137599+Mogileeswaran@users.noreply.github.com>
2020-03-09 13:57:54 +0530

    Create selectSort.cpp

---
+++ selectSort.cpp
@@ -0,0 +1,34 @@
+
+//////          SELECT SORT         ///////////////////
+
+#include <iostream>
+using namespace std;
+void print(int arr[],int s){
+    for(int i=0;i<s;i++)
+        cout<<arr[i]<<" ";
+}
+void selectSort(int arr[],int s){
+    int minimumElement;
+    for(int i=0;i<s;i++){
+         minimumElement=i;
+        for(int j=i+1;j<s;j++){
+           if(arr[j]<arr[minimumElement]){
+              minimumElement=j;
+           }
+        }
+        int temp=arr[i];
+        arr[i]=arr[minimumElement];
+        arr[minimumElement]=temp;
+    }
+}
+int main(){
+    int arr[]={1,3,5,7,9,2,4,6,8,0};
+    int s= sizeof(arr)/ sizeof(arr[0]);
+    print(arr,s);
+    selectSort(arr,s);
+    cout<<endl;
+    print(arr,s);
+    return 0;
+}
+ 
+
